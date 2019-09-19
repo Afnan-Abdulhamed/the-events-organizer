0a15a97c4a3f750953463dd893a3161bffa2879c
unknown <afnanabdulhamed@gmail.com>
2019-09-19 04:17:52 +0200

    rename loader file

--- web/app/plugins/the-events-organizer/includes/class-wpeo-loader.php
+++ web/app/plugins/the-events-organizer/includes/class-wpeo-loader.php
@@ -21,7 +21,7 @@
  * @subpackage Plugin_Name/includes
  * @author     Your Name <email@example.com>
  */
-class Plugin_Name_Loader {
+class WPEO_Loader {
 
 	/**
 	 * The array of actions registered with WordPress.
