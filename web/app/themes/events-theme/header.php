7a0e4f2012f1041e5680aa04d954e188177cfe2c
Afnan-Abdulhamed <afnanabdulhamed@gmail.com>
2019-09-20 23:51:25 +0200

    remove unused code

--- web/app/themes/events-theme/header.php
+++ web/app/themes/events-theme/header.php
@@ -29,21 +29,6 @@
 				 <img src=" <?php echo get_template_directory_uri()?> /assets/images/logo.png"> 
         	</a>
 			
-			<?php
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$events_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $events_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $events_theme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
